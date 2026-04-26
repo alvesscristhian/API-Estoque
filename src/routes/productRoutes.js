179c66ad76599218a264b78999b15317ed584ebb
Cristhian Alves <cristhian.ads@hotmail.com>
2026-04-25 23:43:28 -0300

    feat: implementa rotas básicas para o controlador de produtos

---
+++ src/routes/productRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import productController from '../controllers/ProductController';
+
+const router = new Router();
+
+router.get('/', productController.index);
+// router.get('/:id', productController.show);
+// router.post('/', productController.store);
+// router.put('/:id', productController.update);
+// router.delete('/:id', productController.delete);
+
+export default router;
